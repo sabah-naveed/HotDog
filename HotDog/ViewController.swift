225f07e12c405e06dea849d81462a382700103ce
Sabah Naveed <sabahnaveed03@gmail.com>
2022-04-03 19:09:58 -0400

    Initial Commit

---
+++ HotDog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotDog
+//
+//  Created by Sabah Naveed on 4/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
